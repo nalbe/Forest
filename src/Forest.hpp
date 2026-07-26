e4c20fa238db6a5cfec1a9a11102f10a75c05aab
nalbe666 <nalbe@ya.ru>
2026-07-26 15:05:31 +0300

    Add files via upload

---
+++ src/Forest.hpp
@@ -0,0 +1,39 @@
+﻿// Forest.hpp
+#pragma once
+
+// External headers
+#include "IntegralProxy/IntegralProxy.hpp"
+
+// Implementation-specific headers
+#include "Forest/Traits.hpp"
+#include "Forest/Node.hpp"
+#include "Forest/TraversalPolicy.hpp"
+#include "Forest/NodeRef.hpp"
+#include "Forest/DepthMixin.hpp"
+#include "Forest/IteratorVerifier.hpp"
+#include "Forest/Iterator.hpp"
+#include "Forest/Container.hpp"
+
+
+
+namespace cst
+{
+
+	// =======================================================================
+	//  ForestTraits - forest node traits
+	// =======================================================================
+	template <typename T>
+	using ForestTraits = cst::detail::Forest::Traits<T>;
+
+
+
+	// =======================================================================
+	//  Forest - node-based forest-like container
+	// =======================================================================
+	template <typename T, typename TTraits = cst::detail::Forest::Traits<T>>
+	using Forest = cst::detail::Forest::Container<T, TTraits>;
+
+}  // namespace cst
+
+
+
